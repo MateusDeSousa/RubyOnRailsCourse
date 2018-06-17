0e60036908b700a7f1187730c549b468e69b3bdb
Mateus de Sousa <mateus27sousa@outlook.com>
2018-06-17 01:05:00 +0000

    add template sm-admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando as CATEGORIAS..."
+categories =  [ "Animais e acessorios", 
+								"Esportes",
+								"Para sua casa",
+								"Eletronicos e celulares",
+								"Musica e hobbies",
+								"Bebês e crianças",
+								"Moda e beleza",
+								"Veiculos e barcos",
+								"Imovéis",
+								"Empregos e negócios"]
+
+categories.each do |category|
+	Category.find_or_create_by(description: category)
+end
+
+puts "CATEGORIAS cadastradas!!!"
